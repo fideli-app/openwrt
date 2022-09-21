9e65dffdbc276cb996cea14d487dd0c686437bdf
Huafu Gandon <huafu.gandon@gmail.com>
2022-09-21 05:20:07 +0200

    misc: script to update feeds

---
+++ scripts/update-feeds.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+CUSTOM_REPO_NAME=huafu
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+for pkg in feeds/"${CUSTOM_REPO_NAME}"/*; do
+    if [ -d "${pkg}" ]; then
+        name="$( basename "${pkg}" )"
+        ./scripts/feeds uninstall "${name}"
+        ./scripts/feeds install -p "${CUSTOM_REPO_NAME}" "${name}"
+    fi
+done
